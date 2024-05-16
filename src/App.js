f95dbec1210a834b9d06674392af354dccb7430e
Kreesta <kreesta@localhost.localdomain>
2024-05-16 06:13:13 -0400

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
